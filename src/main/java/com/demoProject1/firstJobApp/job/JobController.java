6c2f5ea940aedefa7773574d89ee3c4b1c248063
adarsshsuman333 <adaeshsuman003@gmail.com>
2026-09-08 20:40:02 +0530

    Adding new jobs

--- src/main/java/com/demoProject1/firstJobApp/job/JobController.java
+++ src/main/java/com/demoProject1/firstJobApp/job/JobController.java
@@ -2,10 +2,31 @@ package com.demoProject1.firstJobApp.job;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+
+/*
+GET /jobs: Get all jobs
+GET /jobs/{id}: Get a specific job by ID
+POST /jobs: Create a new job (request body should contain the job details)
+DELETE /jobs/{id}: Delete a specific job by ID
+PUT /jobs/{id}: Update a specific job by ID (request body should contain the updated job details)
+GET /jobs/{id}/company: Get the company associated with a specific job by ID
+
+Example API URLs:
+GET {base_url}/jobs
+GET {base_url}/jobs/1
+POST {base_url}/jobs
+DELETE {base_url}/jobs/1
+PUT {base_url}/jobs/1
+GET {base_url}/jobs/1/company
+
+*
+*/
 
 @RestController
 public class JobController {
@@ -14,5 +35,11 @@ public class JobController {
     @GetMapping("/jobs")
     public List<Job> findAll(){
         return jobs;
+    }
+
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job){
+        jobs.add(job);
+        return "Job added successfully.";
     }
 }
