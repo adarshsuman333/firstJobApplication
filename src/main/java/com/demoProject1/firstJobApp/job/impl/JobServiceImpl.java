6a1c8958d5eaebd2bc2d63444f81ee7698446831
adarsshsuman333 <adaeshsuman003@gmail.com>
2026-09-12 14:14:14 +0530

    Making Jobs Persistent

--- src/main/java/com/demoProject1/firstJobApp/job/impl/JobServiceImpl.java
+++ src/main/java/com/demoProject1/firstJobApp/job/impl/JobServiceImpl.java
@@ -1,58 +1,59 @@
 package com.demoProject1.firstJobApp.job.impl;
 
 import com.demoProject1.firstJobApp.job.Job;
+import com.demoProject1.firstJobApp.job.JobRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobServiceImpl implements JobService{
 
-    private List<Job> jobs = new ArrayList<>();
+//    private List<Job> jobs = new ArrayList<>();
     Long jobId = 1L;
+
+    JobRepository jobRepository;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
 
     @Override
     public List<Job> findAll() {
-        return jobs;
+        return jobRepository.findAll();
     }
 
     @Override
     public void createJob(Job job) {
         job.setId(jobId++);
-        jobs.add(job);
+        jobRepository.save(job);
     }
 
     @Override
     public Job getJobById(Long id) {
-        for (Job job : jobs) {
-            if (job.getId().equals(id)){
-                return job;
-            }
-        }
-        return null;
+       return jobRepository.findById(id).orElse(null);
     }
 
     @Override
     public boolean deleteJobById(Long id) {
-        Iterator<Job> iterator = jobs.iterator();
-        while (iterator.hasNext()) {
-            Job job = iterator.next();
-            if (job.getId().equals(id)){
-                iterator.remove();
-                return true;
-            }
+        try{
+            jobRepository.deleteById(id);
+            return true;
+        } catch(Exception e){
+            return false;
         }
-        return false;
+
     }
 
     @Override
     public boolean updateJobById(Long id, Job updatedJob) {
-        Iterator<Job> iterator = jobs.iterator();
-        while (iterator.hasNext()) {
-            Job job = iterator.next();
-            if (job.getId().equals(id)){
+        Optional<Job> optionalJob = jobRepository.findById(id);
+            if (optionalJob.isPresent()){
+                Job job = optionalJob.get();
                 job.setTitle(updatedJob.getTitle());
                 job.setId(updatedJob.getId());
                 job.setMaxSalary(updatedJob.getMaxSalary());
@@ -60,7 +61,7 @@ public class JobServiceImpl implements JobService{
                 job.setLocation(updatedJob.getLocation());
                 return true;
             }
-        }
         return false;
+        }
+
     }
-}
