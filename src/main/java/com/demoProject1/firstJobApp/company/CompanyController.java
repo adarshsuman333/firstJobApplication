7b853c41b772122fc2070bd4249709ed784ef2aa
adarsshsuman333 <adaeshsuman003@gmail.com>
2026-09-16 16:58:06 +0530

    Getting all changes

---
+++ src/main/java/com/demoProject1/firstJobApp/company/CompanyController.java
@@ -0,0 +1,25 @@
+package com.demoProject1.firstJobApp.company;
+
+import com.demoProject1.firstJobApp.company.impl.CompanyService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+
+    private CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping
+    public List<Company> getAllCompanies() {
+        return companyService.getAllCompanies();
+    }
+
+}
---
+++ src/main/java/com/demoProject1/firstJobApp/company/impl/CompanyService.java
@@ -0,0 +1,9 @@
+package com.demoProject1.firstJobApp.company.impl;
+
+import com.demoProject1.firstJobApp.company.Company;
+
+import java.util.List;
+
+public interface CompanyService {
+    List<Company> getAllCompanies();
+}
