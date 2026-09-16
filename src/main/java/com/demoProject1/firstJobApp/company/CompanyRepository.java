a530aca81690037158b79a54d6ab5b04844b1afb
adarsshsuman333 <adaeshsuman003@gmail.com>
2026-09-16 16:58:06 +0530

    Getting all companies

---
+++ src/main/java/com/demoProject1/firstJobApp/company/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.demoProject1.firstJobApp.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long>
+{
+}
